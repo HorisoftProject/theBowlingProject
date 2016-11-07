dc7e118233504770fb730ec61fae09e9e68a792a
Abdou Ly <abdouly@users.noreply.github.com>
2016-11-07 10:43:42 +0100

    first commit project

---
+++ src/main/java/dcll/maly/App.java
@@ -0,0 +1,13 @@
+package dcll.maly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
